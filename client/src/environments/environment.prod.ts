fb81526bdb8fae3db719dc27386d7e18dd14c0e2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-28 13:20:44 +0200

    Setting up firebase

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAOlhnbdpDCfQjhIsoIGcwXNVoEKGZRNLg',
+    authDomain: 'property-managment-api.firebaseapp.com',
+    databaseURL:
+      'https://property-managment-api-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'property-managment-api',
+    storageBucket: 'property-managment-api.appspot.com',
+    messagingSenderId: '580933107282',
+    appId: '1:580933107282:web:f9ecc0da7150c5e5abe98b',
+  },
 };

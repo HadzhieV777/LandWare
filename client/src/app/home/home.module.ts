a1148399edc9122cfb3bb875c60857f798c78021
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-11-30 13:04:21 +0200

    Edited home.module.ts

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HeaderComponent, FooterComponent, LandingComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent, LandingComponent],
 })
-export class HomeModule { }
+export class HomeModule {}

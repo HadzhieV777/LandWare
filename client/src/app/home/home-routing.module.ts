76590a0d2e2070d10ed0dd18418e6358209f5b2e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-02 09:47:46 +0200

    Edited home-routing.module.ts

--- client/src/app/home/home-routing.module.ts
+++ client/src/app/home/home-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
-import { ServicesComponent } from './pages/services/services.component';
 
 const routes: Routes = [
     {
-      path: 'services',
-      component: ServicesComponent,
+      path: 'pricing',
+      component: PricingComponent,
     },
     {
       path: 'about',

8a66ed086466e19cce6ca52fe70848758dc30fc6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-02 09:48:02 +0200

    Edited home.module.ts

--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { LandingComponent } from './landing/landing.component';
-import { ServicesComponent } from './pages/services/services.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
 import { UserRoutingModule } from './home-routing.module';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LandingComponent, ServicesComponent, AboutComponent, ResourcesComponent],
+  declarations: [HeaderComponent, FooterComponent, LandingComponent, PricingComponent, AboutComponent, ResourcesComponent, PricingComponent],
   imports: [CommonModule, UserRoutingModule],
   exports: [HeaderComponent, FooterComponent, LandingComponent],
 })

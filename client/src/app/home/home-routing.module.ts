2ecdbe19c9e7ebeef01bb7efca8ef14783032656
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-05 10:21:19 +0200

    Edited home-routing.module

--- client/src/app/home/home-routing.module.ts
+++ client/src/app/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutComponent } from './pages/about/about.component';
 import { PricingComponent } from './pages/pricing/pricing.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
 
@@ -8,10 +7,6 @@ const routes: Routes = [
     {
       path: 'pricing',
       component: PricingComponent,
-    },
-    {
-      path: 'about',
-      component: AboutComponent,
     },
     {
       path: 'resources',
@@ -24,4 +19,4 @@ const routes: Routes = [
     imports: [RouterModule.forChild(routes)],
     exports: [RouterModule],
   })
-  export class UserRoutingModule {}
+  export class HomeRoutingModule {}

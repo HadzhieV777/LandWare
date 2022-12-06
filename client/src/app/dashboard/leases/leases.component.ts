1a74bb7d443837baadb9c93d1daeed6a09843bdd
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-06 09:21:24 +0200

    Generated leases component

---
+++ client/src/app/dashboard/leases/leases.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-leases',
+  templateUrl: './leases.component.html',
+  styleUrls: ['./leases.component.scss']
+})
+export class LeasesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

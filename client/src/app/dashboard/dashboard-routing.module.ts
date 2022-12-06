90681ab63b21f830dfa3c449b7faf8c2c66349b5
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-06 09:18:44 +0200

    Edited dashboard-routing.module.ts

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { FormsComponent } from './forms/forms.component';
+import { LeasesComponent } from './leases/leases.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
 
 const routes: Routes = [
     {
       path: '',
-      component: UserDashboardComponent,
+      component: UserPanelComponent,
+    },
+    {
+      path: 'forms',
+      component: FormsComponent,
+    },
+    {
+      path: 'leases',
+      component: LeasesComponent,
+    },
+    {
+      path: 'expenses',
+      component: ExpensesComponent,
+    },
+    {
+      path: 'leases',
+      component: LeasesComponent,
+    },
+    {
+      path: 'statistics',
+      component: StatisticsComponent,
     },
   ];
   

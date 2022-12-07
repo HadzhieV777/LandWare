e09573d0c5a48fa8e053e4190331387d7d05055e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 22:07:26 +0200

    Generated component

---
+++ client/src/app/dashboard/add-expence/add-expence.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-expence',
+  templateUrl: './add-expence.component.html',
+  styleUrls: ['./add-expence.component.scss']
+})
+export class AddExpenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

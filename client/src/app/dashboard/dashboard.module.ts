a23b1b235947f4633cbe50416a33426ec9e61352
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 22:16:30 +0200

    Edited dashboard,module

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { FormsComponent } from './forms/forms.component';
 import { LeasesComponent } from './leases/leases.component';
 import { ExpensesComponent } from './expenses/expenses.component';
-import { StatisticsComponent } from './statistics/statistics.component';
+import { ResourcesComponent } from './resources/resources.component';
+import { AddExpenceComponent } from './add-expence/add-expence.component';
 
 @NgModule({
-  declarations: [UserPanelComponent, UserDashboardComponent, FormsComponent, LeasesComponent, ExpensesComponent, StatisticsComponent],
+  declarations: [UserPanelComponent, UserDashboardComponent, LeasesComponent, ExpensesComponent, ResourcesComponent, AddExpenceComponent],
   imports: [CommonModule, DashboardRoutingModule],
   exports: [UserDashboardComponent],
 })

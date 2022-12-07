433ee4be82a8db5523ca53b8ebf46a68810e38fb
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 22:15:09 +0200

    Edited estate-routing

--- client/src/app/estate/estate-routing.module.ts
+++ client/src/app/estate/estate-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent } from './create/create.component';
 import { EstatesComponent } from './estates/estates.component';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
 
 const routes: Routes = [
     {
       path: '',
       component: EstatesComponent,
+    },
+    {
+      path: 'maintenance',
+      component: MaintenanceComponent,
+    },
+    {
+      path: 'add',
+      component: CreateComponent,
     },
   ];
   

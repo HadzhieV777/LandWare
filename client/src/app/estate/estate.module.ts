ba118f0565bdb96b3f87db20030493c090ccd9f2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 22:14:53 +0200

    Edited estate module

--- client/src/app/estate/estate.module.ts
+++ client/src/app/estate/estate.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EstatesComponent } from './estates/estates.component';
 import { DashboardRoutingModule } from './estate-routing.module';
+import { MaintenanceComponent } from './maintenance/maintenance.component';
+import { CreateComponent } from './create/create.component';
 
 @NgModule({
-  declarations: [EstatesComponent],
+  declarations: [EstatesComponent, MaintenanceComponent, CreateComponent],
   imports: [CommonModule, DashboardRoutingModule],
 })
 export class EstateModule {}

8bf4218be2913dc674680a8ce62f22a8ee461632
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-07 22:09:36 +0200

    Edited auth-routing.module

--- client/src/app/user/auth-routing.module.ts
+++ client/src/app/user/auth-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './register/register.component';
 
 
@@ -12,6 +13,10 @@ const routes: Routes = [
     {
       path: 'register',
       component: RegisterComponent,
+    },
+    {
+      path: 'profile',
+      component: ProfileComponent,
     },
   ];
   

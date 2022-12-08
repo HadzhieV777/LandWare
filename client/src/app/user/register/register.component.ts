4a5a726fc1c063a8d1dacc67a4d82b58d434e773
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-08 22:04:06 +0200

    Edited register.component

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators, } from '@angular/forms';
+import { CustomvalidationService } from 'src/app/shared/validators/customvalidation.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RegisterComponent {
+  
+  registerForm = this.fb.group(
+    {
+      email: ['', [Validators.required, Validators.email]],
+      fName: ['', [Validators.required, Validators.minLength(2)]],
+      lName: ['', [Validators.required, Validators.minLength(2)]],
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          this.customValidator.patternValidator(),
+        ]),
+      ],
+      rePassword: ['', [Validators.required]],
+    },
+    {
+      validator: this.customValidator.MatchPassword(
+        'password',
+        'rePassword'
+      ),
+    }
+  );
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: CustomvalidationService
+  ) {}
+}
+function matchPasswordsValidator(arg0: string, arg1: string): any {
+  throw new Error('Function not implemented.');
 }

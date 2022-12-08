f584f3428feee60e546d24475161ee636cf4cddd
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-08 22:03:17 +0200

    Added leases routing

---
+++ client/src/app/leases/leases-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AddLeaseComponent } from './add-lease/add-lease.component';
+import { LeasesComponent } from './leases/leases.component';
+
+
+
+const routes: Routes = [
+    {
+      path: '',
+      component: LeasesComponent,
+    },
+    {
+      path: 'add/lease',
+      component: AddLeaseComponent,
+    },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
+  })
+  export class LeasesRoutingModule {}

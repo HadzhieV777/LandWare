48fb80b2335764945ace46f3a101910fe879dd5d
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-09 10:07:36 +0200

    Edited shared module

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { EmailDirective } from './validators/email.directive';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [EmailDirective],
+  imports: [CommonModule],
+  exports: [EmailDirective],
 })
-export class SharedModule { }
+export class SharedModule {}

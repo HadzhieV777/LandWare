6e9d6113948863fd60c30ebd739b525a43b1075f
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-09 10:10:24 +0200

    Edited estate component

--- client/src/app/estate/create/create.component.ts
+++ client/src/app/estate/create/create.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  createPropForm = this.fb.group({
+    propAddress: ['', [Validators.required, Validators.minLength(10)]],
+    city: ['', [Validators.required, Validators.minLength(2)]],
+    zip: ['', [Validators.required, Validators.minLength(2)]],
+    bedrooms: ['', [Validators.required]],
+    baths: ['', [Validators.required]],
+    type: ['', [Validators.required, Validators.minLength(10)]],
+    price: ['', [Validators.required]],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }

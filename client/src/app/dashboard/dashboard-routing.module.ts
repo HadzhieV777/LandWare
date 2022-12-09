3fa22b2ce49b646fd038fc448080cf03cdcd99b4
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-09 10:13:55 +0200

    Edited dashboard-routing

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddExpenceComponent } from './add-expence/add-expence.component';
-import { AddLeaseComponent } from './add-lease/add-lease.component';
 import { ExpensesComponent } from './expenses/expenses.component';
-import { LeasesComponent } from './leases/leases.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 
@@ -12,30 +10,17 @@ const routes: Routes = [
       path: '',
       component: UserPanelComponent,
     },
-    
-    {
-      path: 'leases',
-      component: LeasesComponent,
-    },
     {
       path: 'expenses',
       component: ExpensesComponent,
-    },
-    {
-      path: 'leases',
-      component: LeasesComponent,
-    },
-    {
-      path: 'resources',
-      component: ResourcesComponent,
     },
     {
       path: 'add/expence',
       component: AddExpenceComponent,
     },
     {
-      path: 'add/lease',
-      component: AddLeaseComponent,
+      path: 'resources',
+      component: ResourcesComponent,
     },
   ];
   

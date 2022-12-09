b9c14d1c6c8dc380f873da8ab2debd7ac37eeda1
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-09 15:00:19 +0200

    Integrating firebase

--- client/src/app/user/auth.service.ts
+++ client/src/app/user/auth.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(
+    public afAuth: AngularFireAuth // Inject Firebase auth service
+  ) {}
+  // Register with email/password
+  Register(email: string, password: string) {
+    return this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        window.alert('You have been successfully registered!');
+        console.log(result.user);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+  
+  // Login with email/password
+  Login(email: string, password: string) {
+    return this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
 
-  constructor() { }
+  // Logout
+  Logout() {
+    return this.afAuth.signOut().then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      window.alert(error.message);
+    });
+  }
 }

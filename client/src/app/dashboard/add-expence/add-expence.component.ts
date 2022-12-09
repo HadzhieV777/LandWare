9ed649a93df2fd61a544dbd611cb08b1b2fcdb07
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-09 10:13:27 +0200

    Edited add-expence cmp

--- client/src/app/dashboard/add-expence/add-expence.component.ts
+++ client/src/app/dashboard/add-expence/add-expence.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-expence',
   templateUrl: './add-expence.component.html',
-  styleUrls: ['./add-expence.component.scss']
+  styleUrls: ['./add-expence.component.scss'],
 })
-export class AddExpenceComponent implements OnInit {
+export class AddExpenceComponent  {
+  addExpenceForm = this.fb.group({
+    expenceTitle: ['', [Validators.required, Validators.minLength(5)]],
+    expenceCost: ['', [Validators.required]],
+    propAddress: ['', [Validators.required, Validators.minLength(10)]],
+    expenceDesc: ['', [Validators.required], Validators.minLength(5)],
+  });
+  constructor(private fb: FormBuilder) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }

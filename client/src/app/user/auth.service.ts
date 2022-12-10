034ad52464d8317047e388ae7c3c2666847e4771
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-10 22:57:04 +0200

    Edited auth.service

--- client/src/app/user/auth.service.ts
+++ client/src/app/user/auth.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+// import { Database, ref, set } from '@angular/fire/database';
+import { IUser } from '../shared/models/user.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(
-    public afAuth: AngularFireAuth // Inject Firebase auth service
+    public afs: AngularFirestore,
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
   ) {}
   // Register with email/password
-  Register(email: string, password: string) {
+  Register(email: string, fName: string, lName: string, password: string) {
     return this.afAuth
       .createUserWithEmailAndPassword(email, password)
       .then((result) => {
-        window.alert('You have been successfully registered!');
-        console.log(result.user);
+        this.SetUserData(result.user)
+        // window.alert('You have been successfully registered!');
+        // console.log(result.user);
       })
       .catch((error) => {
         window.alert(error.message);
       });
   }
-  
+
   // Login with email/password
   Login(email: string, password: string) {
     return this.afAuth
@@ -33,12 +41,29 @@ export class AuthService {
       });
   }
 
+  // Set user Data
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData: IUser = {
+      uid: user.uid,
+      email: user.email,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+
   // Logout
   Logout() {
-    return this.afAuth.signOut().then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      window.alert(error.message);
-    });
+    return this.afAuth
+      .signOut()
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
   }
 }

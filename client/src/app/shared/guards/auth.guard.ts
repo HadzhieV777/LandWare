f18113f6034af3a5c41b7297dca6d3093de47dde
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-11 17:57:56 +0200

    Implemented guards

---
+++ client/src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth-service.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(public authService: AuthService, public router: Router) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (this.authService.isLoggedIn !== true) {
+      if (localStorage.getItem('user')) {
+        return true;
+      }
+      this.router.navigate(['/auth/login']);
+    }
+    return true;
+  }
+}

cc66e7842a4dbd07fb01d5c5c265fb47d9c6f20f
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-11 16:11:27 +0200

    Refractoring services dir

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './shared/services/auth-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
-
   isLogedIn = false;
+  constructor(public authService: AuthService) {
+
+  }
+
 }

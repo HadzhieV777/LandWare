8c0b7e9891d443eac6c34ca75dd1f369bc64a5a6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-11 16:07:55 +0200

    Implemented google login

--- client/src/app/user/register/register.component.ts
+++ client/src/app/user/register/register.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
 import { CustomvalidationService } from 'src/app/shared/validators/customvalidation.service';
-import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -13,8 +13,6 @@ export class RegisterComponent {
   registerForm = this.fb.group(
     {
       email: ['', [Validators.required, Validators.email]],
-      fName: ['', [Validators.required, Validators.minLength(2)]],
-      lName: ['', [Validators.required, Validators.minLength(2)]],
       password: [
         '',
         Validators.compose([
@@ -31,8 +29,8 @@ export class RegisterComponent {
   constructor(
     private fb: FormBuilder,
     private customValidator: CustomvalidationService,
-    private authService: AuthService,
-    private router: Router,
+    public authService: AuthService,
+    private router: Router
   ) {}
 
   register(): void {
@@ -40,10 +38,9 @@ export class RegisterComponent {
       return;
     }
 
-    const { email, fName, lName, password } = this.registerForm.value;
-
-    this.authService.Register(email, fName, lName, password);
-    this.router.navigate(['/dashboard'])
+    const { email, password } = this.registerForm.value;
+    this.authService.Register(email, password);
+    this.router.navigate(['/dashboard']);
   }
 }
 function matchPasswordsValidator(arg0: string, arg1: string): any {

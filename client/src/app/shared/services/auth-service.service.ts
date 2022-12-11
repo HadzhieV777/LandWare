0f9cbeb7c4e2660e797dcaac9a7be32c521c5a6b
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-11 16:05:46 +0200

    Added services dir

---
+++ client/src/app/shared/services/auth-service.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/compat/firestore';
+import { Router } from '@angular/router';
+import * as auth from 'firebase/auth';
+import { IUser } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  userData: any; // Save logged in user data
+
+  constructor(
+    public afs: AngularFirestore,
+    public afAuth: AngularFireAuth, // Inject Firebase auth service
+    public router: Router
+  ) {
+    this.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.userData = user;
+        localStorage.setItem('user', JSON.stringify(this.userData));
+        JSON.parse(localStorage.getItem('user')!);
+      } else {
+        localStorage.setItem('user', 'null');
+        JSON.parse(localStorage.getItem('user')!);
+      }
+    });
+  }
+
+  get isLoggedIn(): boolean {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user !== null ? true : false;
+  }
+
+  get user(): IUser {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    return user;
+  }
+
+  // Register with email/password
+  Register(email: string, password: string) {
+    return this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  // Login with email/password
+  Login(email: string, password: string) {
+    return this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.SetUserData(result.user);
+        this.afAuth.authState.subscribe((user) => {
+          if (user) {
+            this.router.navigate(['/dashboard']);
+          }
+        });
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+
+  // Set user Data
+  SetUserData(user: any) {
+    const userRef: AngularFirestoreDocument<IUser> = this.afs.doc(
+      `users/${user.uid}`
+    );
+    const userData: IUser = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+    };
+    return userRef.set(userData, {
+      merge: true,
+    });
+  }
+
+  GoogleAuth() {
+    return this.AuthLogin(new auth.GoogleAuthProvider()).then((res: any) => {
+      this.router.navigate(['/dashboard']);
+    });
+  }
+
+  AuthLogin(provider: any) {
+    return this.afAuth
+      .signInWithPopup(provider)
+      .then((result) => {
+        this.router.navigate(['/dashboard']);
+
+        this.SetUserData(result.user);
+      })
+      .catch((error) => {
+        window.alert(error);
+      });
+  }
+
+  // Logout
+  Logout() {
+    return this.afAuth
+      .signOut()
+      .then(() => {
+        localStorage.removeItem('user');
+        this.router.navigate(['/auth/login']);
+      })
+      .catch((error) => {
+        window.alert(error.message);
+      });
+  }
+}

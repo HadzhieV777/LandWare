dad83e41974e8241a234fd9767bae307eb675f5c
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 21:55:58 +0200

    Edited estates component

--- client/src/app/estate/estates/estates.component.ts
+++ client/src/app/estate/estates/estates.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs';
 import { Estate } from 'src/app/shared/models/estate.model';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
 import { EstateService } from 'src/app/shared/services/estate.service';
 
 @Component({
@@ -11,15 +12,19 @@ import { EstateService } from 'src/app/shared/services/estate.service';
 export class EstatesComponent implements OnInit {
   arrayEstate?: Estate[];
 
-  constructor(private estateService: EstateService) {}
+  constructor(
+    private estateService: EstateService,
+    private authService: AuthService) {}
 
   ngOnInit(): void {
     this.retrieveEstates();
   }
 
+  userId = this.authService.user.uid
+
   retrieveEstates(): void {
     this.estateService
-      .getAll()
+      .getByUser(this.userId)
       .snapshotChanges()
       .pipe(
         map((changes) =>

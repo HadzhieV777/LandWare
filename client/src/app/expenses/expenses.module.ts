caba972808c3ef33fe83461ff222203622c66da6
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 22:16:44 +0200

    Refractoring expenses module

--- client/src/app/expenses/expenses.module.ts
+++ client/src/app/expenses/expenses.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ExpensesComponent } from './expenses/expenses.component';
+import { AddExpenceComponent } from './add-expence/add-expence.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ExpensesRoutingModule } from './expenses-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ExpensesComponent, AddExpenceComponent],
+  imports: [CommonModule, ReactiveFormsModule, ExpensesRoutingModule],
 })
-export class ExpensesModule { }
+export class ExpensesModule {}

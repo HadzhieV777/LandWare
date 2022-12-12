47bb677d18bdcaab09fafc6edcfbae4f62bf9d8a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 17:04:12 +0200

    Integrating CRUD service for estate

--- client/src/app/estate/estates/estates.component.ts
+++ client/src/app/estate/estates/estates.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { Estate } from 'src/app/shared/models/estate.model';
+import { EstateService } from 'src/app/shared/services/estate.service';
 
 @Component({
   selector: 'app-estates',
   templateUrl: './estates.component.html',
-  styleUrls: ['./estates.component.scss']
+  styleUrls: ['./estates.component.scss'],
 })
 export class EstatesComponent implements OnInit {
+  arrayEstate?: Estate[];
 
-  constructor() { }
+  constructor(private estateService: EstateService) {}
 
   ngOnInit(): void {
+    this.retrieveEstates();
   }
 
+  retrieveEstates(): void {
+    this.estateService
+      .getAll()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((e) => ({
+            ...(e.payload.doc.data() as Estate),
+            id: e.payload.doc.id,
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.arrayEstate = data;
+      });
+  }
 }

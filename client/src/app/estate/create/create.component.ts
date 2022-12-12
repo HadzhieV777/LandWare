5993811e6f78c05e6a29000711539745d45ece87
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 17:02:43 +0200

    Edited create.component

--- client/src/app/estate/create/create.component.ts
+++ client/src/app/estate/create/create.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { EstateService } from 'src/app/shared/services/estate.service';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
-  createPropForm = this.fb.group({
-    propAddress: ['', [Validators.required, Validators.minLength(10)]],
-    city: ['', [Validators.required, Validators.minLength(2)]],
-    zip: ['', [Validators.required, Validators.minLength(2)]],
-    bedrooms: ['', [Validators.required]],
-    baths: ['', [Validators.required]],
-    type: ['', [Validators.required, Validators.minLength(10)]],
-    price: ['', [Validators.required]],
-  });
-  constructor(private fb: FormBuilder) {}
+
+  public createPropForm;
+  
+  constructor(
+    private fb: FormBuilder,
+    private estateService: EstateService,
+    private authService: AuthService,
+    private router: Router) {
+      this.createPropForm = this.fb.group({
+        address: ['', [Validators.required, Validators.minLength(10)]],
+        city: ['', [Validators.required, Validators.minLength(2)]],
+        zip: ['', [Validators.required, Validators.minLength(2)]],
+        bedrooms: ['', [Validators.required]],
+        baths: ['', [Validators.required]],
+        type: ['', [Validators.required, Validators.minLength(10)]],
+        price: ['', [Validators.required]],
+      });
+    
+    }
 
   ngOnInit(): void {}
+
+
+  addEstate() {
+    const estate = {
+      owner: this.authService.user.uid,
+      ...this.createPropForm.value,
+      rented: true,
+      maintenance: false
+    }
+    
+    this.estateService.create(estate as any)
+    this.router.navigate(['/properties/'])
+  }
+
 }

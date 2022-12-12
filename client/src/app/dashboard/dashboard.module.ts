111df63f37175cdc486aaa638e672fb25ae334c2
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 22:15:31 +0200

    Refractoring expenses module

--- client/src/app/dashboard/dashboard.module.ts
+++ client/src/app/dashboard/dashboard.module.ts
@@ -3,18 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
-import { ExpensesComponent } from './expenses/expenses.component';
 import { ResourcesComponent } from './resources/resources.component';
-import { AddExpenceComponent } from './add-expence/add-expence.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     UserPanelComponent,
     UserDashboardComponent,
-    ExpensesComponent,
     ResourcesComponent,
-    AddExpenceComponent,
   ],
   imports: [CommonModule, DashboardRoutingModule, ReactiveFormsModule],
   exports: [UserDashboardComponent],

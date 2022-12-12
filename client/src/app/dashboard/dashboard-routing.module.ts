c2b27fd4f35af3d426c52e5c0205db32151a7942
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-12 22:15:23 +0200

    Refractoring expenses module

--- client/src/app/dashboard/dashboard-routing.module.ts
+++ client/src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddExpenceComponent } from './add-expence/add-expence.component';
-import { ExpensesComponent } from './expenses/expenses.component';
 import { ResourcesComponent } from './resources/resources.component';
 import { UserPanelComponent } from './user-panel/user-panel.component';
 
@@ -9,14 +7,6 @@ const routes: Routes = [
     {
       path: '',
       component: UserPanelComponent,
-    },
-    {
-      path: 'expenses',
-      component: ExpensesComponent,
-    },
-    {
-      path: 'add/expence',
-      component: AddExpenceComponent,
     },
     {
       path: 'resources',

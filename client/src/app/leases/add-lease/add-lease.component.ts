ca0c6819bd9c5fcbb209b12b8e9924f9d59a1927
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-13 15:19:26 +0200

    Edited add-lease

--- client/src/app/leases/add-lease/add-lease.component.ts
+++ client/src/app/leases/add-lease/add-lease.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { LeasesService } from 'src/app/shared/services/leases.service';
 
 @Component({
   selector: 'app-add-lease',
@@ -7,13 +10,31 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./add-lease.component.scss'],
 })
 export class AddLeaseComponent {
-  addLeaseForm = this.fb.group({
-    leaseTitle: ['', [Validators.required, Validators.minLength(5)]],
-    propAddress: ['', [Validators.required, Validators.minLength(10)]],
-    rent: ['', [Validators.required]],
-    leaseDesc: ['', [Validators.required], Validators.minLength(5)],
-    dateFrom: [''],
-    dateTo: [''],
-  });
-  constructor(private fb: FormBuilder) {}
+  public addLeaseForm;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private leasesService: LeasesService
+  ) {
+    this.addLeaseForm = this.fb.group({
+      startDate: [''].toString(),
+      endDate: [''].toString(),
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+      rent: ['', [Validators.required]],
+      description: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
+
+  addLease() {
+  const lease = {
+    owner: this.authService.user.uid,
+    ...this.addLeaseForm.value,
+  };
+
+  this.leasesService.create(lease as any);
+  this.router.navigate(['/leases']);
+}
 }

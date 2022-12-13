d7969a9a5a0ea182e98da79acba13ea62ea77ce0
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-13 10:43:06 +0200

    Edited expenses

--- client/src/app/expenses/expenses/expenses.component.ts
+++ client/src/app/expenses/expenses/expenses.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { Expence } from 'src/app/shared/models/expence.model';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { ExpencesService } from 'src/app/shared/services/expences.service';
 
 @Component({
   selector: 'app-expenses',
   templateUrl: './expenses.component.html',
-  styleUrls: ['./expenses.component.scss']
+  styleUrls: ['./expenses.component.scss'],
 })
 export class ExpensesComponent implements OnInit {
+  arrayExpenses?: Expence[];
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private expensesService: ExpencesService
+  ) {}
 
   ngOnInit(): void {
+    this.retrieveExpenses();
   }
 
+  userId = this.authService.user.uid;
+
+  retrieveExpenses(): void {
+    this.expensesService
+      .getByUser(this.userId)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((e) => ({
+            ...(e.payload.doc.data() as Expence),
+            id: e.payload.doc.id,
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.arrayExpenses = data;
+      });
+  }
 }

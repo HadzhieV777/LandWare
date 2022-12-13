30d7255398ebb96fdf7e7b5e68a43d1eb914bd61
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-13 10:42:43 +0200

    Edited add-expence

--- client/src/app/expenses/add-expence/add-expence.component.ts
+++ client/src/app/expenses/add-expence/add-expence.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { ExpencesService } from 'src/app/shared/services/expences.service';
 
 @Component({
   selector: 'app-add-expence',
   templateUrl: './add-expence.component.html',
   styleUrls: ['./add-expence.component.scss'],
 })
-export class AddExpenceComponent  {
-  addExpenceForm = this.fb.group({
-    expenceTitle: ['', [Validators.required, Validators.minLength(5)]],
-    expenceCost: ['', [Validators.required]],
-    propAddress: ['', [Validators.required, Validators.minLength(10)]],
-    expenceDesc: ['', [Validators.required], Validators.minLength(5)],
-  });
-  constructor(private fb: FormBuilder) {}
+export class AddExpenceComponent {
+  public addExpenceForm;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private expensesService: ExpencesService
+  ) {
+    this.addExpenceForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(5)]],
+      cost: ['', [Validators.required]],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+      description: ['', [Validators.required, Validators.minLength(5)]],
+    });
+  }
 
+  addExpense() {
+    const expense = {
+      owner: this.authService.user.uid,
+      ...this.addExpenceForm.value,
+    };
+
+    this.expensesService.create(expense as any);
+    this.router.navigate(['/expenses']);
+  }
 }

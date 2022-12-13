251c5abf8f721a1f3eff5ebfea6618031cf12388
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-13 15:18:52 +0200

    Edited leases.component

--- client/src/app/leases/leases/leases.component.ts
+++ client/src/app/leases/leases/leases.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { Lease } from 'src/app/shared/models/lease.model';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { LeasesService } from 'src/app/shared/services/leases.service';
 
 @Component({
   selector: 'app-leases',
   templateUrl: './leases.component.html',
-  styleUrls: ['./leases.component.scss']
+  styleUrls: ['./leases.component.scss'],
 })
 export class LeasesComponent implements OnInit {
+  arrayLeases?: Lease[];
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private leaseService: LeasesService
+  ) {}
 
   ngOnInit(): void {
+    this.retrieveExpenses();
   }
 
+  userId = this.authService.user.uid;
+
+  retrieveExpenses(): void {
+    this.leaseService
+      .getByUser(this.userId)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((e) => ({
+            ...(e.payload.doc.data() as Lease),
+            id: e.payload.doc.id,
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.arrayLeases = data;
+      });
+  }
 }

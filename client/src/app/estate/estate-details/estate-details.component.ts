2ca9c6b6e214d74fb27f3a2f071002ac2038c4af
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-14 16:06:37 +0200

    Edited estate-details component

--- client/src/app/estate/estate-details/estate-details.component.ts
+++ client/src/app/estate/estate-details/estate-details.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
 import { Estate } from 'src/app/shared/models/estate.model';
 import { EstateService } from 'src/app/shared/services/estate.service';
 
@@ -9,31 +14,73 @@ import { EstateService } from 'src/app/shared/services/estate.service';
   templateUrl: './estate-details.component.html',
   styleUrls: ['./estate-details.component.scss'],
 })
-export class EstateDetailsComponent {
-  // estate: Estate | undefined;
+export class EstateDetailsComponent implements OnInit {
+  @Input() estate?: Estate;
+  @Output() refreshList: EventEmitter<any> = new EventEmitter();
+  currentEstate: Estate = {
+    address: '',
+    city: '',
+    zip: '',
+    bedrooms: 0,
+    baths: 0,
+    type: '',
+    price: 0,
+    rented: true,
+    maintenance: false,
+  };
+  message = '';
+  showEditMode = false;
+  formSubmitted = false;
 
-  // constructor(
-  //   private estateService: EstateService,
-  //   private activatedRoute: ActivatedRoute
-  // ) {
-  //   this.fetchSingleEstate();
-  // }
+  constructor(private estateService: EstateService) {}
 
-  // fetchSingleEstate(): void {
-  //   this.estate = undefined;
-  //   const estateId = this.activatedRoute.snapshot.params['id'];
-  //   const estateAr = this.estateService.getById(estateId);
-  //   console.log(
-  //     estateAr.snapshotChanges().pipe(
-  //       map((changes) =>
-  //         changes.map((e) => ({
-  //           ...(e.payload.doc.data() as Estate),
-  //           id: e.payload.doc.id,
-  //         }))
-  //       )
-  //     ).subscribe((data) => {
-  //       console.log(data);
-  //     })
-  //   );
-  // }
+  ngOnInit(): void {}
+
+  ngOnChanges(): void {
+    this.message = '';
+    this.currentEstate = { ...this.estate };
+  }
+
+  updateEstate(): void {
+    const data = {
+      id: this.currentEstate.id,
+      owner: this.currentEstate.owner,
+      address: this.currentEstate.address,
+      city: this.currentEstate.city,
+      zip: this.currentEstate.zip,
+      bedrooms: this.currentEstate.bedrooms,
+      baths: this.currentEstate.baths,
+      type: this.currentEstate.type,
+      price: this.currentEstate.price,
+      rented: this.currentEstate.rented,
+      maintenance: this.currentEstate.maintenance,
+    };
+
+    if (this.currentEstate.id) {
+      this.estateService
+        .update(this.currentEstate.id, data)
+        .then(() => (this.message = 'The estate was updated successfully!'))
+        .catch((err) => console.log(err));
+    }
+  }
+
+  deleteEstate(): void {
+    if (this.currentEstate.id) {
+      this.estateService
+        .delete(this.currentEstate.id)
+        .then(() => {
+          this.refreshList.emit();
+          this.message = 'The estate was deleted successfully!';
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+
+  toggleEditMode(): void {
+    this.showEditMode = !this.showEditMode;
+    if (this.showEditMode) {
+      this.formSubmitted = false;
+      // this.editProfileForm.reset();
+    }
+  }
 }

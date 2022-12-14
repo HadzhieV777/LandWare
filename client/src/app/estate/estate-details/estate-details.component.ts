f569cb1701808dd0bbc458553f0f5e46e7577bec
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-14 10:49:22 +0200

    Edited estate-details

--- client/src/app/estate/estate-details/estate-details.component.ts
+++ client/src/app/estate/estate-details/estate-details.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { Estate } from 'src/app/shared/models/estate.model';
+import { EstateService } from 'src/app/shared/services/estate.service';
 
 @Component({
   selector: 'app-estate-details',
   templateUrl: './estate-details.component.html',
-  styleUrls: ['./estate-details.component.scss']
+  styleUrls: ['./estate-details.component.scss'],
 })
-export class EstateDetailsComponent implements OnInit {
+export class EstateDetailsComponent {
+  // estate: Estate | undefined;
 
-  constructor() { }
+  // constructor(
+  //   private estateService: EstateService,
+  //   private activatedRoute: ActivatedRoute
+  // ) {
+  //   this.fetchSingleEstate();
+  // }
 
-  ngOnInit(): void {
-  }
-
+  // fetchSingleEstate(): void {
+  //   this.estate = undefined;
+  //   const estateId = this.activatedRoute.snapshot.params['id'];
+  //   const estateAr = this.estateService.getById(estateId);
+  //   console.log(
+  //     estateAr.snapshotChanges().pipe(
+  //       map((changes) =>
+  //         changes.map((e) => ({
+  //           ...(e.payload.doc.data() as Estate),
+  //           id: e.payload.doc.id,
+  //         }))
+  //       )
+  //     ).subscribe((data) => {
+  //       console.log(data);
+  //     })
+  //   );
+  // }
 }

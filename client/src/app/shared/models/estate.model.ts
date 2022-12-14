64cae021f6484e38f956a54904782f111f9c0de7
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-14 16:07:35 +0200

    Edited estate model

--- client/src/app/shared/models/estate.model.ts
+++ client/src/app/shared/models/estate.model.ts
@@ -1,13 +1,13 @@
 export class Estate {
   id?: string;
   owner?: string;
-  address!: string;
-  city!: string;
-  zip!: string;
-  bedrooms!: number;
-  baths!: number;
-  type!: string;
-  price!: number;
-  rented!: boolean;
-  maintenance!: boolean;
+  address?: string;
+  city?: string;
+  zip?: string;
+  bedrooms?: number;
+  baths?: number;
+  type?: string;
+  price?: number;
+  rented?: boolean;
+  maintenance?: boolean;
 }

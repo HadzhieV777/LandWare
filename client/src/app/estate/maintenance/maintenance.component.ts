931307ec1ef8013b4bc4ff52c288b1f96e778b17
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-15 22:27:17 +0200

    Implemented maintenance logic

--- client/src/app/estate/maintenance/maintenance.component.ts
+++ client/src/app/estate/maintenance/maintenance.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { map } from 'rxjs';
+import { Estate } from 'src/app/shared/models/estate.model';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { EstateService } from 'src/app/shared/services/estate.service';
 
 @Component({
   selector: 'app-maintenance',
   templateUrl: './maintenance.component.html',
-  styleUrls: ['./maintenance.component.scss']
+  styleUrls: ['./maintenance.component.scss'],
 })
-export class MaintenanceComponent implements OnInit {
+export class MaintenanceComponent {
+  arrayFiltered?: Estate[];
+  userId = this.authService.user.uid;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private estateService: EstateService,
+    private authService: AuthService
+  ) {
+    this.filterEstate();
   }
 
+  filterEstate(): void {
+    this.estateService
+      .getByUser(this.userId)
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((e) => ({
+            ...(e.payload.doc.data() as Estate),
+            id: e.payload.doc.id,
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.arrayFiltered = data.filter((e) => e.maintenance === true);
+      });
+  }
 }

4e0c93507c6df4bb172027d4dddf8988da00070a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-16 13:41:59 +0200

    Added animations

--- client/src/app/shared/animations/animations.ts
+++
@@ -1,9 +0,0 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
-
-export let fade = trigger('fade', [
-      state('void', style({opacity: 0})),
-      transition('void <=> *', [
-        style({opacity: 0}),
-        animate(2000)
-      ]),
-    ])

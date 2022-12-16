3eb3666f5588d3585cd56a9b13246ca776b4998e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-16 13:41:21 +0200

    Added animations

--- client/src/app/estate/maintenance/maintenance.component.ts
+++ client/src/app/estate/maintenance/maintenance.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { map } from 'rxjs';
+import { listAnimation } from 'src/app/shared/animations/list-animations';
 import { Estate } from 'src/app/shared/models/estate.model';
 import { AuthService } from 'src/app/shared/services/auth-service.service';
 import { EstateService } from 'src/app/shared/services/estate.service';
@@ -8,6 +9,7 @@ import { EstateService } from 'src/app/shared/services/estate.service';
   selector: 'app-maintenance',
   templateUrl: './maintenance.component.html',
   styleUrls: ['./maintenance.component.scss'],
+  animations: [listAnimation]
 })
 export class MaintenanceComponent {
   arrayFiltered?: Estate[];

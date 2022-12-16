d4402ed2c400a3cf53b2897db58184d699370502
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-16 13:39:28 +0200

    Added animations

---
+++ client/src/app/shared/animations/fade-animations.ts
@@ -0,0 +1,16 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+const enterTransition =  transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({opacity: 1})),
+])     
+
+const exitTransition = transition(':leave', [
+  style({opacity: 1}),
+  animate('1s ease-out', style({opacity: 0}))
+])
+
+export let fadeIn = trigger('fadeIn', [enterTransition])
+export let fadeOut = trigger('fadeOut', [exitTransition])

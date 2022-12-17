fcab9e4dd5b19486fba493e57e4875121e11fd5a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-17 13:14:00 +0200

    Added animations

--- client/src/app/estate/create/create.component.ts
+++ client/src/app/estate/create/create.component.ts
@@ -3,45 +3,45 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { EstateService } from 'src/app/shared/services/estate.service';
 import { AuthService } from 'src/app/shared/services/auth-service.service';
+import { fadeIn } from 'src/app/shared/animations/fade-animations';
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
+animations: [fadeIn]
 })
 export class CreateComponent implements OnInit {
-
   public createPropForm;
-  
+
   constructor(
     private fb: FormBuilder,
     private estateService: EstateService,
     private authService: AuthService,
-    private router: Router) {
-      this.createPropForm = this.fb.group({
-        address: ['', [Validators.required, Validators.minLength(10)]],
-        city: ['', [Validators.required, Validators.minLength(2)]],
-        zip: ['', [Validators.required, Validators.minLength(2)]],
-        bedrooms: ['', [Validators.required]],
-        baths: ['', [Validators.required]],
-        type: ['', [Validators.required, Validators.minLength(10)]],
-        price: ['', [Validators.required]],
-      });
-    
-    }
+    private router: Router
+  ) {
+    this.createPropForm = this.fb.group({
+      address: ['', [Validators.required, Validators.minLength(10)]],
+      city: ['', [Validators.required, Validators.minLength(2)]],
+      zip: ['', [Validators.required, Validators.minLength(2)]],
+      bedrooms: ['', [Validators.required]],
+      baths: ['', [Validators.required]],
+      type: ['', [Validators.required, Validators.minLength(10)]],
+      price: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {}
-
 
   addEstate() {
     const estate = {
       owner: this.authService.user.uid,
       ...this.createPropForm.value,
       rented: true,
-      maintenance: false
-    }
-    
-    this.estateService.create(estate as any)
-    this.router.navigate(['/properties/'])
-  }
+      maintenance: false,
+    };
 
+    this.estateService.create(estate as any);
+    this.router.navigate(['/properties/']);
+  }
 }

4247025e5df9ce7359830aaa28f3794bc65d5eb9
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-19 22:15:46 +0200

    Added mobile nav

--- client/src/app/home/header/header.component.ts
+++ client/src/app/home/header/header.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  visible = true;
 
-  constructor() { }
+  constructor(private responsive: BreakpointObserver) {}
 
   ngOnInit(): void {
+    this.responsive.observe([Breakpoints.XSmall]).subscribe((result) => {
+      this.navIsVisible = false;
+
+      if (result.matches) {
+        this.menuIconVar = true;
+      }
+    });
   }
 
+  menuIconVar: boolean = true;
+  navIsVisible: boolean = false;
+
+  openMenu() {
+    this.navIsVisible = !this.navIsVisible;
+    this.menuIconVar = !this.menuIconVar;
+  }
 }

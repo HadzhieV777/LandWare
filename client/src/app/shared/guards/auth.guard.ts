b3f282c234b089efde2e942f0e1f5ad80076bd39
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-19 13:33:30 +0200

    Edited auth guard

--- client/src/app/shared/guards/auth.guard.ts
+++ client/src/app/shared/guards/auth.guard.ts
@@ -18,10 +18,11 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.authService.isLoggedIn) {
+    if (this.authService.isLoggedIn !== true) {
+      this.router.navigate(['auth/login']);
+      return false;
+    } else {
       return true;
     }
-    this.router.navigate(['/auth/login']);
-    return false
   }
 }

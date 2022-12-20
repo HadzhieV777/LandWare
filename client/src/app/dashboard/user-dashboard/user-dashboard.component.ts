ef46813a5b33283ddab55ba93d9e18e57428ed23
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-20 11:55:46 +0200

    Mobile nav for user dashboard

--- client/src/app/dashboard/user-dashboard/user-dashboard.component.ts
+++ client/src/app/dashboard/user-dashboard/user-dashboard.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth-service.service';
-
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-user-dashboard',
   templateUrl: './user-dashboard.component.html',
-  styleUrls: ['./user-dashboard.component.scss']
+  styleUrls: ['./user-dashboard.component.scss'],
 })
 export class UserDashboardComponent implements OnInit {
-
-  constructor(public authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private responsive: BreakpointObserver
+  ) {}
 
   ngOnInit(): void {
+    this.responsive.observe('(min-width: 1024px)').subscribe((result) => {
+      this.hidePointer = false;
+
+      if (result.matches) {
+        this.changeWidth = false;
+        this.hidePointer = true;
+        this.hideContent = false;
+      }
+    });
   }
 
+  changeWidth: boolean = false;
+  hidePointer: boolean = true;
+  hideContent: boolean = false;
+  turnPointer: boolean = true;
+  hideOverlay: boolean = false;
+
+  openMenu() {
+    this.changeWidth = !this.changeWidth;
+    this.hideContent = !this.hideContent;
+    this.turnPointer = !this.turnPointer;
+    this.hideOverlay = !this.hideOverlay;
+  }
 }

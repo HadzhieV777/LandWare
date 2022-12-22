10470e1a1948b3918798bf0e08985960e8a0042a
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-22 14:00:55 +0200

    Fixing animation bug

--- client/src/app/shared/animations/bounce.ts
+++ client/src/app/shared/animations/bounce.ts
@@ -1,8 +1,16 @@
-import { animation, style, animate, keyframes, trigger } from '@angular/animations';
+import {
+  animation,
+  style,
+  animate,
+  keyframes,
+  trigger,
+  transition,
+  useAnimation,
+} from '@angular/animations';
 
 export const DEFAULT_TIMING = '0.5';
 
-export const bounceIn = animation(
+const bInAnimation = animation(
   animate(
     '{{ timing }}s {{ delay }}s cubic-bezier(0.17, 0.89, 0.24, 1.11)',
     keyframes([
@@ -26,18 +34,23 @@ export const bounceIn = animation(
   { params: { timing: DEFAULT_TIMING, delay: 0 } }
 );
 
-export const bounceOut = animation(
+const bOutAnimation = animation(
   animate(
-    '{{ timing }}s {{ delay }}s cubic-bezier(0.6, 0.72, 0, 1)',
+    '{{ timing }}s {{ delay }}s cubic-bezier(0.17, 0.89, 0.24, 1.11)',
     keyframes([
+      style({
+        opacity: 0,
+        transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, 1000px, 0)',
+        offset: 0,
+      }),
       style({
         opacity: 1,
         transform: 'scale3d(0.475, 0.475, 0.475) translate3d(0, -60px, 0)',
-        offset: 0.3,
+        offset: 0.6,
       }),
       style({
-        opacity: 0,
-        transform: 'scale3d(0.1, 0.1, 0.1) translate3d(0, 500px, 0)',
+        opacity: 1,
+        transform: 'scale3d(1, 1, 1) translate3d(0,0,0)',
         offset: 1,
       }),
     ])
@@ -45,5 +58,23 @@ export const bounceOut = animation(
   { params: { timing: DEFAULT_TIMING, delay: 0 } }
 );
 
+const bInTransition = transition(
+  'void => *',
+  useAnimation(bInAnimation, {
+    params: { timing: 0.9 },
+  })
+);
+
+const bOutTransition = transition(
+  '* => void',
+  useAnimation(bOutAnimation, {
+    params: { timing: 0.6 },
+  })
+);
 
 // Fix this animation
+
+export const bounceIn = trigger('bounceIn', [bInTransition]);
+
+export const bounceOut = trigger('boundeOut', [bOutTransition]);
+

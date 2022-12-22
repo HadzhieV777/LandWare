1353db9ba96441c32c885e4bfc7c8fda91957594
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-22 21:22:08 +0200

    Extends landing page sections functionality

---
+++ client/src/app/shared/services/faq.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
+import { Faq } from '../models/faq.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExpencesService {
+
+  private dbPath = '/faq';
+
+  faqRef: AngularFirestoreCollection<Faq>;
+
+  constructor(private db: AngularFirestore) {
+    this.faqRef = db.collection(this.dbPath);
+  }
+  
+  getAll(): AngularFirestoreCollection<Faq> {
+    return this.faqRef;
+  }
+  // create(faq: Faq): any {
+  //   return this.faqRef.add({...faq});
+  // }
+
+  // update(id: string, data: any): Promise<void> {
+  //   return this.faqRef.doc(id).update(data);
+  // }
+
+  // delete(id: string): Promise<void> {
+  //   return this.faqRef.doc(id).delete();
+  // }
+
+}

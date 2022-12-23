2c496b7fddd4cb54facc18e1a9317587433d267e
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-23 14:16:11 +0200

    Refactoring

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { LandingComponent } from './landing/landing.component';
 import { ResourcesComponent } from './pages/resources/resources.component';
 import { CoreRoutingModule } from './core-routing.module';
 import { PricingComponent } from './pages/pricing/pricing.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, LandingComponent, PricingComponent, ResourcesComponent, PricingComponent, NotFoundComponent],
+  declarations: [HeaderComponent, FooterComponent, PricingComponent, ResourcesComponent, PricingComponent, NotFoundComponent],
   imports: [CommonModule, CoreRoutingModule],
-  exports: [HeaderComponent, FooterComponent, LandingComponent],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {}

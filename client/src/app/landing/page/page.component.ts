b79b0bffe9bc05d4e9d79a098e96b178dca18b6f
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-23 11:48:04 +0200

    Edited page component

--- client/src/app/landing/page/page.component.ts
+++ client/src/app/landing/page/page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth-service.service';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss', '../shared.scss']
 })
 export class PageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    if (this.authService.user) {
+      this.router.navigate(['/dashboard']);
+    }
   }
+
+  hideData: boolean = true;
+
+  showHiddenData() {
+    this.hideData = !this.hideData
+  }
+
 
 }

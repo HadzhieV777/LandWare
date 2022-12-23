4b47cdfb795bb81ef1782ce8d2e4692e2e4d2900
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-23 11:48:47 +0200

    Edited landing module

--- client/src/app/landing/landing.module.ts
+++ client/src/app/landing/landing.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageComponent } from './page/page.component';
-
-
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { ServicesSectionComponent } from './services-section/services-section.component';
+import { BenefitsSectionComponent } from './benefits-section/benefits-section.component';
+import { FaqSectionComponent } from './faq-section/faq-section.component';
 
 @NgModule({
-  declarations: [
-    PageComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PageComponent, HeroSectionComponent, ServicesSectionComponent, BenefitsSectionComponent, FaqSectionComponent],
+  imports: [CommonModule],
+  exports: [PageComponent],
 })
-export class LandingModule { }
+export class LandingModule {}

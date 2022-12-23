f015bee0f658af8596553267e04738a2e7ac4499
HadzhieV777 <bdwhadjiev42@gmail.com>
2022-12-23 14:17:18 +0200

    Added toggle functionality

--- client/src/app/landing/faq-section/faq-section.component.ts
+++ client/src/app/landing/faq-section/faq-section.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { fadeIn, fadeOut } from 'src/app/shared/animations/fade-animations';
+import { Faq } from 'src/app/shared/models/faq.model';
+import { FaqService } from 'src/app/shared/services/faq.service';
 
 @Component({
   selector: 'app-faq-section',
   templateUrl: './faq-section.component.html',
-  styleUrls: ['./faq-section.component.scss']
+  styleUrls: ['./faq-section.component.scss'],
+  animations: [fadeIn, fadeOut],
 })
-export class FaqSectionComponent implements OnInit {
+export class FaqSectionComponent {
+  faqArray?: Faq[];
 
-  constructor() { }
+  constructor(
+    // private authService: AuthService,
+    private faqService: FaqService
+  ) {}
 
   ngOnInit(): void {
+    this.retrieveFaq();
   }
 
+  retrieveFaq(): void {
+    this.faqService
+      .getAll()
+      .snapshotChanges()
+      .pipe(
+        map((changes) =>
+          changes.map((e) => ({
+            ...(e.payload.doc.data() as Faq),
+            id: e.payload.doc.id,
+          }))
+        )
+      )
+      .subscribe((data) => {
+        this.faqArray = data;
+      });
+  }
+
+  showHiddenData(index: number) {
+    if (this.faqArray && this.faqArray[index]) {
+      this.faqArray[index].hideData = !this.faqArray[index].hideData;
+    }
+  }
 }

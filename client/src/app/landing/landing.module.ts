bf707daaeadf90f582664cb1249ae0d773d82f56
HadzhieV777 <bdwhadjiev42@gmail.com>
2023-01-04 16:27:55 +0200

    Added routing module

--- client/src/app/landing/landing.module.ts
+++ client/src/app/landing/landing.module.ts
@@ -5,10 +5,11 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { ServicesSectionComponent } from './services-section/services-section.component';
 import { BenefitsSectionComponent } from './benefits-section/benefits-section.component';
 import { FaqSectionComponent } from './faq-section/faq-section.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [PageComponent, HeroSectionComponent, ServicesSectionComponent, BenefitsSectionComponent, FaqSectionComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [PageComponent],
 })
 export class LandingModule {}
